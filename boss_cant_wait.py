0829f1687b9b6789d211e1f33aaf4c79d7d3a4ac
Owen K <owenkarongadirect@gmail.com>
2024-10-07 12:54:37 +0300

    Added model evaluation function

--- boss_cant_wait.py
+++ boss_cant_wait.py
@@ -85,6 +85,29 @@ def plot_predictions(
     # Show the legend
     plt.legend(prop={"size": 14})
 
+#Feel free to evaluate your model
+def eval_model(model: torch.nn.Module,
+               data_loader:torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               accuracy_fn):
+
+  loss, acc = 0, 0
+  model.eval()
+  with torch.inference_mode():
+    for X,y in tqdm(data_loader):
+      y_pred = model(X)
+
+      loss += loss_fn(y_pred, y)
+      acc += accuracy_fn(y_true=y,
+                         y_pred=y_pred.argmax(dim=1))
+
+    loss /= len(data_loader)
+    acc /= len(data_loader)
+
+  return {"model_name": model.__class__.__name__, # only works when model is created with class
+          "model_loss": loss.item(),
+          "model_acc": acc}
+
 
 # Calculate accuracy (a classification metric)
 def accuracy_fn(y_true, y_pred):
